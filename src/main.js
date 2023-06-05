1572dd676510ec2cacad38e97a417d75f848a8e9
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-06-05 09:14:46 -0400

    Randomly update matrix every second

--- src/main.js
+++ src/main.js
@@ -1,97 +1,44 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { matrix, updateMatrix } from './tetris.js';
 
-// need a scene, camera, renderer
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 
 camera.position.setX(1.66);
 camera.position.setY(7.5);
 camera.position.setZ(38);
 
-// console.log(camera.position.x); // X-coordinate
-// console.log(camera.position.y); // Y-coordinate
-// console.log(camera.position.z); // Z-coordinate
-// console.log(camera.rotation.x)
-// console.log(camera.rotation.y)
-// console.log(camera.rotation.z)
-
-renderer.render(scene, camera);
-
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
-
 const ambientLight = new THREE.AmbientLight(0xffffff);
-
-scene.add(pointLight, ambientLight);
-
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
+
+scene.add(pointLight, ambientLight, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-
-Array(200).fill().forEach(addStar);
-
 function createGrid() {
-  const gridArray = [
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-
-    // ... continue with the remaining rows
-  ];
-
   const cubeSize = 2;
 
-  for (let i = 0; i < gridArray.length; i++) {
-    for (let j = 0; j < gridArray[i].length; j++) {
+  scene.remove(...scene.children.filter(child => child.type === 'Mesh'));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
       const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-      const cubeMaterial = new THREE.MeshStandardMaterial({ color: gridArray[i][j] === 1 ? 0xff0000 : 0x00ff00 });
+      const cubeMaterial = new THREE.MeshStandardMaterial({ color: matrix[i][j] !== 0 ? 0xff0000 : 0x0000ff });
       const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-      cube.position.x = j * cubeSize - (gridArray[i].length * cubeSize) / 2;
-      cube.position.y = -i * cubeSize + (gridArray.length * cubeSize) / 2;
+      cube.position.x = j * cubeSize;
+      cube.position.y = -i * cubeSize;
+      cube.position.z = 0;
 
       scene.add(cube);
     }
@@ -104,8 +51,12 @@ function animate() {
   requestAnimationFrame(animate);
 
   controls.update();
-
   renderer.render(scene, camera);
 }
 
 animate();
+
+setInterval(() => {
+  updateMatrix();
+  createGrid();
+}, 1000);
